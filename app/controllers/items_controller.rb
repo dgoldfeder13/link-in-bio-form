3dec1dcf81ec8146d2268479acc0328f5749f786
dgoldfeder13 <114775261+dgoldfeder13@users.noreply.github.com>
2025-02-18 18:53:51 +0000

    erorr manage redirect

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,14 @@ class ItemsController < ApplicationController
   def create
     render({ :template => "item_templates/backdoor"})
   end
+
+  def item_create
+    i = Item.new
+    i.link_url = params.fetch("link_url_param")
+    i.link_description = params.fetch("link_description_param")
+    i.thumbnail_url = params.fetch("thumbnail_url_param")
+    i.save
+    #render({ :template => "item_templates/new_item_midway"})
+    redirect_to("/")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "items", :action => "index" })
   
+  post("/new_item", { :controller => "items", :action => "item_create" })
+
   get("/backdoor", { :controller => "items", :action => "create"})
 end
